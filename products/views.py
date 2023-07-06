af6b86f33dec6b405fab85a0c673f3eb73564062
Mohammadreza Gharghabi <mohammadreza.gharghabi6@gmail.com>
2023-07-06 12:22:55 +0330

    implementation pagination for comments in product_detail page

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
+from django.core.paginator import Paginator
 from django.views import View
-from django.shortcuts import get_object_or_404, render, redirect, reverse
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import Http404, JsonResponse
 from django.contrib import messages
 from django.utils.translation import gettext as _
@@ -41,6 +42,29 @@ class ProductDetailView(View):
         recent_visits.add_product(product)
         comments = product.comments.order_by("-modified_datetime")
 
+        paginator = Paginator(comments, 2)
+        page = request.GET.get("page")
+        page_obj = paginator.get_page(page)
+
+        LAST_PAGE_NUMBER = page_obj.paginator.num_pages
+        BEFORE_AND_AFTER_PAGE_COUNT = 1
+        list_range_pages = range(1, LAST_PAGE_NUMBER + 1)
+        list_show_pages = [1, page_obj.paginator.num_pages]
+
+        if LAST_PAGE_NUMBER - (2 * BEFORE_AND_AFTER_PAGE_COUNT) > 2 * BEFORE_AND_AFTER_PAGE_COUNT:
+            if page_obj.number == 1:
+                list_show_pages[1:1] = [2, 3]
+            elif page_obj.number == LAST_PAGE_NUMBER:
+                list_show_pages[-1:-1] = [LAST_PAGE_NUMBER - 2, LAST_PAGE_NUMBER - 1]
+            else:
+                list_between_pages = []
+                for index in range(-BEFORE_AND_AFTER_PAGE_COUNT, BEFORE_AND_AFTER_PAGE_COUNT + 1):
+                    if page_obj.number + index not in list_show_pages:
+                        list_between_pages.append(page_obj.number + index)
+                list_show_pages[1:1] = list_between_pages
+        else:
+            list_show_pages = list_range_pages
+
         if request.user.is_authenticated:
             comment = Comment.objects.filter(user=request.user, product=product)
             form = CommentForm(instance=comment.first()) if comment.exists() else CommentForm()
@@ -48,7 +72,9 @@ class ProductDetailView(View):
             form = CommentForm()
 
         return render(request, "products/product_detail.html",
-                      context={"product": product, "form": form, "comments": comments})
+                      context={"product": product, "form": form, "comments": comments,
+                               "page_obj": page_obj, "list_show_pages": list_show_pages,
+                               "range_pages": list_range_pages})
 
     def post(self, request, *args, **kwargs):
         category = get_object_or_404(Category, category_name=kwargs["category_name"])
